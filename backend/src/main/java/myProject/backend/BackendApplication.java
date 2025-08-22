8ffc79a58fb95eab7dc75fc674269d06ed6d54f6
Kim S.J <tkdwns0016@naver.com>
2025-08-23 02:50:46 +0900

    김상준 : BE / git 분리 커밋 적용 테스트

---
+++ backend/src/main/java/myProject/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package myProject.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan("myProject.backend.entity")
+public class BackendApplication {
+
+	public static void main(String[] args) { SpringApplication.run(BackendApplication.class, args);	}
+
+}
---
+++ backend/src/main/java/myProject/backend/service/NoticeService.java
@@ -0,0 +1,25 @@
+package myProject.backend.service;
+
+import myProject.backend.entity.Notice;
+import myProject.backend.repository.NoticeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class NoticeService {
+
+
+}	private final NoticeRepository noticeRepository;
+
+
+public NoticeService(NoticeRepository noticeRepository) {
+	this.noticeRepository = noticeRepository;
+}
+
+public List<Notice> not( ) {
+	return noticeRepository.findAllByOrderByNoticeId();
+}
+
